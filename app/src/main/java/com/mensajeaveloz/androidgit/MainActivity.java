f9d3858bdad40d1b2c8e7cfa8fb7a8d2781a8f68
jorgeluishm2019 <49533878+jorgeluishm2019@users.noreply.github.com>
2019-04-26 20:27:13 -0800

    INTENT-FILTER

--- app/src/main/java/com/mensajeaveloz/androidgit/MainActivity.java
+++ app/src/main/java/com/mensajeaveloz/androidgit/MainActivity.java
@@ -1,13 +1,31 @@
 package com.mensajeaveloz.androidgit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton=(Button) findViewById(R.id.button);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Intent intent = new Intent(this,Main2Activity.class);
+                startActivity(intent);
+                break;
+
+             default:
+                 break;
+        }
     }
 }
