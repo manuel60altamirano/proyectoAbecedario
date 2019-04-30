8a621af4fce1bcf85498d174fd7150eace2dad4c
jorgeluishm2019 <49533878+jorgeluishm2019@users.noreply.github.com>
2019-04-30 11:07:20 -0800

    Ciclo de vida de un servicio.

--- app/src/main/java/com/mensajeaveloz/androidgit/Servicio.java
+++ app/src/main/java/com/mensajeaveloz/androidgit/Servicio.java
@@ -6,23 +6,31 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class Servicio extends Service {
     private MediaPlayer mediaPlayer;
     private Context thisContext=this;
     @Override
     public void onCreate(){
+        Toast.makeText(this, "Servicio Creado!",Toast.LENGTH_SHORT).show();
+        mediaPlayer=MediaPlayer.create(thisContext,R.raw.music);
 
     }
     @Override
     public int onStartCommand(Intent intent,int flag ,int idProcess){
-        mediaPlayer=MediaPlayer.create(thisContext,R.raw.music);
+        Toast.makeText(this,"Servicio Iniciado!",Toast.LENGTH_SHORT).show();
         mediaPlayer.start();
-        return START_STICKY;
+
+        return super.onStartCommand(intent, flag, idProcess);
     }
+
+    
     @Override
     public void onDestroy(){
-        mediaPlayer.stop();
+        Toast.makeText(this,"Servicio Destruido!",Toast.LENGTH_SHORT).show();
+        mediaPlayer.release();
+        //mediaPlayer.stop();
     }
     @Nullable
     @Override
