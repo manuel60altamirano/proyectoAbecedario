14b7c74a96ba21293973773927c5b8ed678f5596
carlosae64 <shawnae64@gmail.com>
2019-05-01 22:24:00 -0400

    /Codigo json Usuario/ /Carlos Almaraz/

--- app/src/main/java/com/mensajeaveloz/androidgit/MainActivity.java
+++ app/src/main/java/com/mensajeaveloz/androidgit/MainActivity.java
@@ -7,17 +7,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button boton;
     private Context thisContext=this;
     private Button BtnIniciar;
     private Button BtnDetener;
+    String JSON_STRING = "{\"usuario\":{\"nombre\":\"Pedro Perez\",\"codigo\":65000}}";
+    String nombre, codigo;
+    TextView usuarioNombre, usuarioCodigo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Teniendo la informacion de json Carlos Almaraz
+        usuarioNombre = (TextView) findViewById(R.id.nombre);
+        usuarioCodigo = (TextView) findViewById(R.id.codigo);
+
+        try {
+            // Tomando JSONObject from JSON file
+            JSONObject obj = new JSONObject(JSON_STRING);
+            // fetch JSONObject named employee
+            JSONObject usuario = obj.getJSONObject("usuario");
+            // get employee name and salary
+            name = usuario.getString("nombre");
+            salary = usuario.getString("codigo");
+            // set employee name and salary in TextView's
+            usuarioNombre.setText("Nombre: "+nombre);
+            usuarioCodigo.setText("Codigo: "+codigo);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //codigo json usuario
         boton=(Button) findViewById(R.id.button);
         boton.setOnClickListener(this);
 
