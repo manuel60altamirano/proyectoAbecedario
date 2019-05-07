e6ff5227f154f3910f1d2b88dd1ce351b7113a40
ALEXANDER CALLE <joelalexandercalle@gmal.com>
2019-05-07 12:40:18 -0400

    SHARED PREFERENCES

--- app/src/main/java/com/mensajeaveloz/androidgit/MainActivity.java
+++ app/src/main/java/com/mensajeaveloz/androidgit/MainActivity.java
@@ -2,12 +2,15 @@ package com.mensajeaveloz.androidgit;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import org.json.JSONException;
@@ -23,10 +26,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView usuarioNombre, usuarioCodigo;
 
 
+    /////preferencias
+    EditText etdato;
+    Button btnguardar, btnmostrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Context context = this;
+        final SharedPreferences sharpref = getSharedPreferences("archivosp",context.MODE_PRIVATE);
+        etdato = (EditText) findViewById(R.id.etdato);
+        btnmostrar = (Button) findViewById(R.id.btnmostrar);
+        btnguardar = (Button) findViewById(R.id.btnguardar);
+
+        btnmostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharpref =getPreferences(Context.MODE_PRIVATE);
+                String valor= sharpref.getString("midato","NO HAY DATOS GUARDADOS");
+                Toast.makeText(getApplicationContext(),"DATO GUARDADO: "+valor,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharpref =getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor =sharpref.edit();
+                editor.putString("midato",etdato.getText().toString());
+                editor.commit();
+
+            }
+        });
+
+
         // Teniendo la informacion de json Carlos Almaraz
         usuarioNombre = (TextView) findViewById(R.id.nombre);
         usuarioCodigo = (TextView) findViewById(R.id.codigo);
@@ -36,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             JSONObject obj = new JSONObject(JSON_STRING);
             JSONObject usuario = obj.getJSONObject("usuario");
             // poner nombre usuario y codigo
-            usuario = usuario.getString("nombre");
+            nombre = usuario.getString("nombre");
             codigo = usuario.getString("codigo");
             // poner nombre y codigo en el  TextView's
             usuarioNombre.setText("Nombre: "+nombre);
